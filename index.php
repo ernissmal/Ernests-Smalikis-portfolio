c34b3d8493901f4ad8c197d167065e69268f44b1
ernissmal <79010059+ernissmal@users.noreply.github.com>
2023-10-01 16:09:21 +0300

    index.php file structure
    Creating the file structure for the index.php file

---
+++ index.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ernests Smalikis Portfolio</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+</head>
+<body>
+    <header>
+        <div class="siteTitle">
+            <h1>Ernests Smaliķis</h1>
+        </div>
+        <div class="navbar">
+            <button>About</button>
+            <button>Home</button>
+            <button>Portfolio</button>
+            <div class="dropdown">
+                <button>Open Dropdown</button>
+                <div class="dropdown-content">
+                    <a href="">Item 1</a>
+                    <a href="#">Item 2</a>
+                    <a href="#">Item 3</a>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section id="about">
+        <div class="profileImage">
+            <img src="profile.jpg" alt="profile image">
+        </div>
+        <div class="information">
+            <div class="icons">
+                <div class="experience">
+
+                </div>
+                <div class="values">
+
+                </div>
+                <div class="approach">
+
+                </div>
+                <div class="passion">
+
+                </div>
+            </div>
+            <div class="generalInformation">
+                <div class="education">
+
+                </div>
+                <div class="work">
+
+                </div>
+            </div>
+            <div class="social">
+                <h1>Follow me on social media</h1>
+                <div class="twitter">
+                    <!--Icon-->
+                    <p>Twitter/X</p>
+                </div>
+                <div class="youtube">
+                    <!--Icon-->
+                    <p>YouTube</p>
+                </div>
+                <div class="instagram">
+                    <!--Icon-->
+                    <p>Instagram</p>
+                </div>
+                <div class="tiktok">
+                    <!--Icon-->
+                    <p>TikTok</p>
+                </div>
+            </div>
+        </div>
+
+
+    </section>
+
+    <section id="portfolio">
+        <h1>Latest work</h1>
+        <div class="project1">
+            <div class=project1-images>
+                <img src="" alt="main-image">
+                <div class="project1-additionalImages">
+                    <img src="" alt="Additional image 1">
+                    <img src="" alt="Additional image 2">
+                </div>
+            </div>
+            <div class="project1Description">
+                <h2>Project name</h2>
+                <p>Project description</p>
+            </div>
+        </div>
+
+        <div class="project2">
+            <div class=project2-images>
+                <img src="" alt="main-image">
+                <div class="project2-additionalImages">
+                    <img src="" alt="Additional image 1">
+                    <img src="" alt="Additional image 2">
+                </div>
+            </div>
+            <div class="project2Description">
+                <h2>Project name</h2>
+                <p>Project description</p>
+            </div>
+        </div>
+    </section>
+
+    <section id="services">
+        <h1>services</h1>
+        <table>
+            <tr>
+                <h2>Service name</h2>
+                <h3>Price</h3>
+                <div class="Service1Description">
+                    <div class="service1DescriptionPoint1">
+                        <!--Icon-->
+                        <p>Point 1</p>
+                    </div>
+                    <div class="service1DescriptionPoint2">
+                        <!--Icon-->
+                        <p>Point 2</p>
+                    </div>
+                    <div class="service1DescriptionPoint3">
+                        <!--Icon-->
+                        <p>Point 3</p>
+                    </div>
+                    <div class="service1DescriptionPoint4">
+                        <!--Icon-->
+                        <p>Point 4</p>
+                    </div>
+                    <div class="service1ApplyNowBtn">
+                        <!--Icon-->
+                        <button>Apply now</button>
+                    </div>
+                </div>
+            </tr>
+
+            <tr>
+                <h2>Service name</h2>
+                <h3>Price</h3>
+                <div class="Service2Description">
+                    <div class="service2DescriptionPoint1">
+                        <!--Icon-->
+                        <p>Point 1</p>
+                    </div>
+                    <div class="service2DescriptionPoint2">
+                        <!--Icon-->
+                        <p>Point 2</p>
+                    </div>
+                    <div class="service2DescriptionPoint3">
+                        <!--Icon-->
+                        <p>Point 3</p>
+                    </div>
+                    <div class="service2DescriptionPoint4">
+                        <!--Icon-->
+                        <p>Point 4</p>
+                    </div>
+                    <div class="service2ApplyNowBtn">
+                        <!--Icon-->
+                        <button>Apply now</button>
+                    </div>
+                </div>
+            </tr>
+
+            <tr>
+                <h2>Service name</h2>
+                <h3>Price</h3>
+                <div class="Service3Description">
+                    <div class="service3DescriptionPoint1">
+                        <!--Icon-->
+                        <p>Point 1</p>
+                    </div>
+                    <div class="service3DescriptionPoint2">
+                        <!--Icon-->
+                        <p>Point 2</p>
+                    </div>
+                    <div class="service3DescriptionPoint3">
+                        <!--Icon-->
+                        <p>Point 3</p>
+                    </div>
+                    <div class="service3DescriptionPoint4">
+                        <!--Icon-->
+                        <p>Point 4</p>
+                    </div>
+                    <div class="service3ApplyNowBtn">
+                        <!--Icon-->
+                        <button>Apply now</button>
+                    </div>
+                </div>
+            </tr>
+        </table>
+    </section>
+
+    <footer>
+        <div class="copyright">
+            <h1>© Ernests Smaliķis <i>2023</i></h1>
+        </div>
+
+        <div class="copyrightDescription">
+            <i><p>All rights reserved</p></i>
+        </div>
+    </footer>
+</body>
+</html>
