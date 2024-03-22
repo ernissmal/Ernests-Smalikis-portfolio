0e039826a3cb1ab37e251237fc5a11a0e67adc8e
ernissmal <79010059+ernissmal@users.noreply.github.com>
2024-03-22 15:43:03 +0200

    Footer.css

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,22 +1,17 @@
 // Footer.js
 import React from 'react';
-import './Footer.css'; // Assuming you might want to style your footer
+import SocialMedia from './../SocialMedia';
+import './Footer.css';
 
 function Footer() {
   return (
     <footer className="App-footer">
       <div className="footer-content">
         <p>Connect with me on social media:</p>
-        <ul className="social-media-list">
-          <li><a href="https://twitter.com/ESmalikis" target="_blank" rel="noopener noreferrer">Twitter</a></li>
-          <li><a href="https://github.com/ernissmal" target="_blank" rel="noopener noreferrer">GitHub</a></li>
-          <li><a href="https://linkedin.com/in/ernests-smaliķis" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
-          {/* Add more social media links as needed */}
-        </ul>
-        <p>Email: smalikisernests@gmail.com</p>
+        <SocialMedia id="footer"/>
         <p>© {new Date().getFullYear()} Ernests Smalikis. All rights reserved.</p>
       </div>
-      <p id="version-control">Version: 2.0</p>
+      <p id="version-control">Version: 1.4</p>
     </footer>
   );
 }
