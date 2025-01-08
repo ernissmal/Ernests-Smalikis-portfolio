0ab45bdc0066e6ec13f6ca51fee542ecc867deaa
ernissmal <79010059+ernissmal@users.noreply.github.com>
2025-01-08 20:46:18 +0200

    TextBlock component

---
+++ frontend/src/components/TextBlock.vue
@@ -0,0 +1,26 @@
+<template>
+    <BaseBlock :id="id" :section="section">
+      <p :class="textClass">{{ content }}</p>
+    </BaseBlock>
+  </template>
+  
+  <script>
+  import BaseBlock from "./BaseBlock.vue";
+  
+  export default {
+    props: {
+      id: String,
+      section: String,
+      content: {
+        type: String,
+        required: true,
+      },
+      textClass: {
+        type: String,
+        default: "",
+      },
+    },
+    components: { BaseBlock },
+  };
+  </script>
+  
