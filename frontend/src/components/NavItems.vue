36e9ec68fb3602d070c98584c6e847338a0b7235
ernissmal <79010059+ernissmal@users.noreply.github.com>
2025-01-08 16:41:37 +0200

    NavItems development

---
+++ frontend/src/components/NavItems.vue
@@ -0,0 +1,32 @@
+<template>
+    <ul class="navItems">
+        <li><a href="#about">About</a></li>
+        <li><a href="#portfolio">Latest project</a></li>
+        <li><a href="#contacts">Contacts</a></li>
+    </ul>
+    <style>
+        .navItems {
+            list-style: none;
+            display: flex;
+            gap: 2rem;
+        }
+
+        .navItems li a {
+            font-size: 1.2rem;
+            font-weight: 500;
+            letter-spacing: 0.05rem;
+            transition: color 0.3s;
+            padding: 200px;
+        }
+
+        .navItems li a:hover {
+            color: #aaa;
+        }
+    </style>
+</template>
+
+<script>
+export default {
+    name: 'NavItems'
+}
+</script>
