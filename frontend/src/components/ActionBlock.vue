64a6e7e1a943a92c38b834051d2617377b529b82
ernissmal <79010059+ernissmal@users.noreply.github.com>
2025-01-08 20:45:38 +0200

    ActionBlock component

---
+++ frontend/src/components/ActionBlock.vue
@@ -0,0 +1,24 @@
+<template>
+    <BaseBlock :id="id" :section="section">
+      <button v-for="(action, index) in actions" :key="index" @click="action.handler">
+        {{ action.label }}
+      </button>
+    </BaseBlock>
+  </template>
+  
+  <script>
+  import BaseBlock from "./BaseBlock.vue";
+  
+  export default {
+    props: {
+      id: String,
+      section: String,
+      actions: {
+        type: Array,
+        required: true, // [{ label: "Click Me", handler: () => alert("Clicked!") }]
+      },
+    },
+    components: { BaseBlock },
+  };
+  </script>
+  
