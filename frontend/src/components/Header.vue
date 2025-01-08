48ac60b59e736695c436c134f3ec775881f9ec1f
ernissmal <79010059+ernissmal@users.noreply.github.com>
2025-01-08 20:33:06 +0200

    Header component development and styling

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,57 +1,35 @@
 <template>
-    <header>
-        <nav>
-            <div class="logo">
-                <img src="../assets/vue.svg" alt="Website's logo" height="10%" width="10%">
-            </div>
-            <div class="navItems">
-                <a href="#about">About</a>
-                <a href="#portfolio">Latest project</a>
-                <a href="#contacts">Contacts</a>
-            </div>
-            <div class="social">
-                <a href="https://twitter.com/ESmalikis"><i class="fab fa-twitter"></i></a>
-                <a href="https://www.instagram.com/erenesto_smalikis"><i class="fab fa-instagram"></i></a>
-                <a href="https://www.youtube.com/@ESmalikis"><i class="fab fa-youtube"></i></a>
-                <a href="https://github.com/ernissmal"><i class="fab fa-github"></i></a>
-            </div>
-        </nav>
-    </header>
+  <header class="header">
+    <nav class="nav">
+      <div class="logo">
+        <img src="../assets/vue.svg" alt="Website's logo" class="logo-img" />
+      </div>
+      <ul class="nav-items">
+        <li><a href="#about">About</a></li>
+        <li><a href="#portfolio">Latest Project</a></li>
+        <li><a href="#contacts">Contacts</a></li>
+      </ul>
+      <div class="social">
+        <a href="https://twitter.com/ESmalikis" class="social-link">
+          <i class="fab fa-twitter"></i>
+        </a>
+        <a href="https://www.instagram.com/erenesto_smalikis" class="social-link">
+          <i class="fab fa-instagram"></i>
+        </a>
+        <a href="https://www.youtube.com/@ESmalikis" class="social-link">
+          <i class="fab fa-youtube"></i>
+        </a>
+        <a href="https://github.com/ernissmal" class="social-link">
+          <i class="fab fa-github"></i>
+        </a>
+      </div>
+    </nav>
+  </header>
 </template>
 
 <script>
+import '../assets/css/Header.css';
 export default {
-    name: 'Header'
-}
+  name: "Header",
+};
 </script>
-
-<style scoped>
-.header {
-  position: fixed;
-  top: 0;
-  width: 100%;
-  padding: 1rem 2rem;
-  background: rgba(0, 0, 0, 0.8); /* Transparent black */
-  color: white;
-  z-index: 1000;
-  transition: all 0.3s ease;
-}
-
-.nav-items {
-  list-style: none;
-  display: flex;
-  gap: 2rem;
-}
-
-.nav-items li a {
-  font-size: 1.2rem;
-  font-weight: 500;
-  letter-spacing: 0.05rem;
-  transition: color 0.3s;
-  padding: 200px;
-}
-
-.nav-items li a:hover {
-  color: #aaa;
-}
-</style>
