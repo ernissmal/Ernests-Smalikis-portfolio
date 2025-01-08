72cfc421d878cde62f51617ec89a3bb7d6525f2f
ernissmal <79010059+ernissmal@users.noreply.github.com>
2025-01-08 20:48:36 +0200

    ContentRenderer component to ensure content rendering across all content blocks

---
+++ frontend/src/components/ContentRenderer.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+      <component
+        v-for="block in blocks"
+        :is="block.type"
+        :key="block.id"
+        v-bind="block.props"
+      />
+    </div>
+  </template>
+  
+  <script>
+  import ActionBlock from "./ActionBlock.vue";
+  import TextBlock from "./TextBlock.vue";
+  import ImageBlock from "./ImageBlock.vue";
+  import DatabaseInputBlock from "./DatabaseInputBlock.vue";
+  
+  export default {
+    components: {
+      ActionBlock,
+      TextBlock,
+      ImageBlock,
+      DatabaseInputBlock,
+    },
+    props: {
+      blocks: {
+        type: Array,
+        required: true, // [{ type: 'ActionBlock', id: '1', section: 'hero', props: { ... } }]
+      },
+    },
+  };
+  </script>
+  
