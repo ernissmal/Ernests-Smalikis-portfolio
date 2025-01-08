40ea963f111b74b83319a9aa81ba840439b1666b
ernissmal <79010059+ernissmal@users.noreply.github.com>
2025-01-08 20:48:01 +0200

    DatabaseInputBlock component

---
+++ frontend/src/components/DatabaseInputBlock.vue
@@ -0,0 +1,44 @@
+<template>
+    <BaseBlock :id="id" :section="section">
+      <form @submit.prevent="submitForm">
+        <label v-for="(field, index) in fields" :key="index">
+          {{ field.label }}
+          <input
+            :type="field.type"
+            :name="field.name"
+            :placeholder="field.placeholder"
+            v-model="formData[field.name]"
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </BaseBlock>
+  </template>
+  
+  <script>
+  import BaseBlock from "./BaseBlock.vue";
+  
+  export default {
+    props: {
+      id: String,
+      section: String,
+      fields: {
+        type: Array,
+        required: true, // [{ label: "Name", type: "text", name: "name", placeholder: "Enter your name" }]
+      },
+    },
+    data() {
+      return {
+        formData: {},
+      };
+    },
+    methods: {
+      submitForm() {
+        console.log(this.formData);
+        // Add form submission logic
+      },
+    },
+    components: { BaseBlock },
+  };
+  </script>
+  
