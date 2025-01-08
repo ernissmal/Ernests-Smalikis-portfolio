51fc28f3eba828889086dc79e14e803ac4aebe70
ernissmal <79010059+ernissmal@users.noreply.github.com>
2025-01-08 20:44:20 +0200

    Developedment of BaseBlock component to improve reusability and scalability

---
+++ frontend/src/components/BaseBlock.vue
@@ -0,0 +1,30 @@
+<template>
+    <div :id="id" :class="section" class="content-block">
+      <slot></slot> <!-- Placeholder for child content -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      id: {
+        type: String,
+        required: true,
+      },
+      section: {
+        type: String,
+        required: true,
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .content-block {
+    margin: 1rem 0;
+    padding: 1rem;
+    border: 1px solid #ccc;
+    border-radius: 8px;
+  }
+  </style>
+  
