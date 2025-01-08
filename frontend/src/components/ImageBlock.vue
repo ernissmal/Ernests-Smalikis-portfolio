6d5aa99e784ad70e344dfe9c3198af1a61209757
ernissmal <79010059+ernissmal@users.noreply.github.com>
2025-01-08 20:47:38 +0200

    ImageBlock component

---
+++ frontend/src/components/ImageBlock.vue
@@ -0,0 +1,35 @@
+<template>
+    <BaseBlock :id="id" :section="section">
+      <img :src="src" :alt="alt" :class="imageClass" />
+      <p v-if="caption">{{ caption }}</p>
+    </BaseBlock>
+  </template>
+  
+  <script>
+  import BaseBlock from "./BaseBlock.vue";
+  
+  export default {
+    props: {
+      id: String,
+      section: String,
+      src: {
+        type: String,
+        required: true,
+      },
+      alt: {
+        type: String,
+        default: "Image",
+      },
+      caption: {
+        type: String,
+        default: "",
+      },
+      imageClass: {
+        type: String,
+        default: "",
+      },
+    },
+    components: { BaseBlock },
+  };
+  </script>
+  
