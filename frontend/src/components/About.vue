7fdad5c10e92de11705e00c7e5678b0af908f04e
ernissmal <79010059+ernissmal@users.noreply.github.com>
2025-01-12 23:28:33 +0200

    Refactor About component to use buttons for section navigation and implement ContentRenderer for dynamic content display

--- frontend/src/components/About.vue
+++ frontend/src/components/About.vue
@@ -1,8 +1,10 @@
 <template>
     <BaseBlock class="about">
-        <Biography />
-        <SkillsList />
-        <ToggleDetails />
+        <div class="buttons">
+            <button @click="setActiveSection('biography')">Biography</button>
+            <button @click="setActiveSection('skillset')">Skillset</button>
+        </div>
+        <ContentRenderer :blocks="activeBlocks" />
     </BaseBlock>
 </template>
 
@@ -11,6 +13,7 @@ import Biography from './Biography.vue';
 import SkillsList from './SkillsList.vue';
 import ToggleDetails from './ToggleDetails.vue';
 import BaseBlock from './BaseBlock.vue';
+import ContentRenderer from './ContentRenderer.vue';
 
 export default {
     name: 'About',
@@ -18,13 +21,45 @@ export default {
         Biography,
         SkillsList,
         ToggleDetails,
-        BaseBlock
+        BaseBlock,
+        ContentRenderer
+    },
+    data() {
+        return {
+            activeSection: 'biography',
+            blocks: {
+                biography: [
+                    { type: 'Biography', id: 'bio1', section: 'biography', props: {} }
+                ],
+                skillset: [
+                    { type: 'SkillsList', id: 'skills1', section: 'skillset', props: {} }
+                ]
+            }
+        };
+    },
+    computed: {
+        activeBlocks() {
+            return this.blocks[this.activeSection];
+        }
+    },
+    methods: {
+        setActiveSection(section) {
+            this.activeSection = section;
+        }
     }
-}
+};
 </script>
 
 <style scoped>
 .about {
     padding: 20px;
+}
+.buttons {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 20px;
+}
+button {
+    margin: 0 10px;
 }
 </style>
