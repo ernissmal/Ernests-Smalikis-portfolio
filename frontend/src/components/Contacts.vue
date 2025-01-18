dcc24e7ed86c0bdcebc2473a8e97e89f7c29c176
ernissmal <79010059+ernissmal@users.noreply.github.com>
2025-01-18 15:38:24 +0200

    Refactor Contacts component to use BaseBlock for address and social sections, enhancing structure and maintainability

--- frontend/src/components/Contacts.vue
+++ frontend/src/components/Contacts.vue
@@ -1,28 +1,35 @@
 <template>
     <div class="contacts">
         <h1>Contact me</h1>
-        <div class="address-card">
+        <BaseBlock id="address-card" section="address">
             <h2>{{ address.name }}</h2>
             <p>{{ address.street }}</p>
             <p>{{ address.city }}, {{ address.state }} {{ address.zip }}</p>
-        </div>
-        <div class="social-icons">
-            <a href="https://www.linkedin.com/in/ernestssmalikis" target="_blank">
-                <i class="fab fa-linkedin"></i>
-            </a>
-            <a href="https://github.com/ernestssmalikis" target="_blank">
-                <i class="fab fa-github"></i>
-            </a>
-            <a href="https://twitter.com/ernestssmalikis" target="_blank">
-                <i class="fab fa-twitter"></i>
-            </a>
-        </div>
+        </BaseBlock>
+        <BaseBlock id="social-icons" section="social">
+            <div class="social-icons">
+                <a href="https://www.linkedin.com/in/ernestssmalikis" target="_blank">
+                    <i class="fab fa-linkedin"></i>
+                </a>
+                <a href="https://github.com/ernestssmalikis" target="_blank">
+                    <i class="fab fa-github"></i>
+                </a>
+                <a href="https://twitter.com/ernestssmalikis" target="_blank">
+                    <i class="fab fa-twitter"></i>
+                </a>
+            </div>
+        </BaseBlock>
     </div>
 </template>
 
 <script>
+import BaseBlock from './BaseBlock.vue';
+
 export default {
     name: 'Contacts',
+    components: {
+        BaseBlock
+    },
     data() {
         return {
             address: {
